ef8791f65de72319136e99ae542262df9d4cbb3a
Thien Tran <tranct@sea.com>
2023-01-23 21:28:58 +0700

    feat: implement createElement

--- example/src/containers/App.jsx
+++ example/src/containers/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-export function createElement() {
-  console.log('createElement');
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+export function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child)
+      )
+    }
+  };
 }
 
 export function render() {
