378a41f52253555b448eeadf13f7a33a18dabee9
Thien Tran <tranct@sea.com>
2023-01-24 08:08:59 +0700

    feat: implement React render

--- src/index.js
+++ src/index.js
@@ -20,8 +20,22 @@ export function createElement(type, props, ...children) {
   };
 }
 
-export function render() {
-  console.log('render');
+export function render(elementOrElementGenerator, parentDom) {
+  const element = typeof elementOrElementGenerator === 'function' ? elementOrElementGenerator() : elementOrElementGenerator;
+
+  const { type, props } = element;
+  // create the dom for current element
+  const dom = type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type);
+
+  const isProperty = key => key !== 'children';
+  Object.keys(props).filter(isProperty).forEach(key => dom[key] = props[key])
+
+  // recursively call render to children
+  element.props.children.forEach(child => {
+    render(child, dom);
+  });
+
+  parentDom.appendChild(dom);
 }
 
 const React = {
