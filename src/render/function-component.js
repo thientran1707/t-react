9ca3f26d534c778405f56f20c772b829259139e3
Thien Tran <tranct@sea.com>
2023-02-19 23:06:10 +0800

    feat: add boilerplate for useState

--- src/render/function-component.js
+++ src/render/function-component.js
@@ -5,7 +5,21 @@ let wipFiber = null;
 let hookIndex = null;
 
 export function useState(initial) {
-  return [initial, () => {}];
+  const oldHook =
+    wipFiber.alternate &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternative.hooks[hookIndex];
+
+  const hook = {
+    // copy the state from previous hook
+    // or use initial if this is the first time to run hook
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  };
+
+  const setState = action => {};
+
+  return [hook.state, setState];
 }
 
 export function updateFunctionComponent(fiber) {
