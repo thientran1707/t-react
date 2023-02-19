a95fc79e78ff8d2136c74348ee4e4a834ade674d
Thien Tran <tranct@sea.com>
2023-02-19 21:56:42 +0800

    feat: update Demo to have event listener

--- example/src/components/SimpleDemo/index.jsx
+++ example/src/components/SimpleDemo/index.jsx
@@ -4,10 +4,19 @@ import Demo1 from './Demo1';
 import Demo2 from './Demo2';
 
 export default function SimpleDemo({ text }) {
+  const [count, setCount] = React.useState(0);
+
+  const onCounterClick = () => {
+    console.log('onCounterClick');
+    setCount(count => count + 1);
+  };
+
   return (
     <div>
       <h2>Simple Demo</h2>
       <h2>{text}</h2>
+      <h2>Count: {count}</h2>
+      <button onClick={onCounterClick}>Click to increase count</button>
       <Demo1 />
       <Demo2 />
     </div>
