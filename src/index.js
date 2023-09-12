2396a16d488c7599671d38effe9d5d64338603ee
Thien Tran <tranct@sea.com>
2023-09-13 01:40:02 +0800

    feat: implement React.createElement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-export function createElement() {
-  console.log('createElement');
+// Special case to handle text node
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+
+export function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(
+        child => typeof child === 'object' ? child : createTextElement(child)
+      ),
+    }
+  }
 }
 
 export function createRoot(parentDom) {
